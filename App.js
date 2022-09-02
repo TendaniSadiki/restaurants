3ece39b528c5714d5bbaa51dee6bc4280489b35e
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-09-02 15:59:38 +0200

    {nav } added

--- App.js
+++ App.js
@@ -13,9 +13,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F6F6F6',
+    
   },
 });
--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,38 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
+import { FaBars } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+
 
 export default function Home() {
   return (
-    <View><Text>Open up App.js to start working on your app!</Text></View>
+    <View style={styles.homeContainer}>
+        <View style={styles.navBar}>
+            <FaBars  style={styles.Bar} />
+            <FaUser  style={styles.User} />
+        </View>
+    </View>
   )
 }
+const styles = StyleSheet.create({
+    homeContainer: {
+      backgroundColor: '#000',
+      flex:1,
+    
+    },
+    navBar: {
+        backgroundColor: 'blue',
+        height:80
+      },
+      Bar: {
+        height:80,
+        color:"#374151",
+        marginLeft:10
+      },
+      User: {
+        height:80,
+        color:"#374151",
+        position:"absolute",
+        right:10
+      },
+  });
