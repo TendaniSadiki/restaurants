e6f726d4256f3b6aaf0a250dec1a92f4318febb2
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-09-02 14:24:12 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View><Text>Open up App.js to start working on your app!</Text></View>
+  )
+}
