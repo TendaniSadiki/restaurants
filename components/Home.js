1bf5a177c3c63e1aaa30f0baea91356836ddef65
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-09-05 10:46:09 +0200

    [input ] added

--- components/Home.js
+++ components/Home.js
@@ -2,21 +2,34 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 import { FaBars } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+import { FaLocationArrow } from 'react-icons/fa';
+import { TextInput } from 'react-native-web';
 
 
 export default function Home() {
   return (
     <View style={styles.homeContainer}>
         <View style={styles.navBar}>
-            <FaBars  style={styles.Bar} />
+          
+                <FaBars  style={styles.Bar}/>
+
+                <View style={styles.locationContent}>
+                    <Text  style={styles.locationText}>Agrabad 435, Chittagong</Text>
+                    <FaLocationArrow style={styles.BarLocation} />
+                </View>
             <FaUser  style={styles.User} />
+        </View>
+        <View style={styles.Search}>
+        <FaSearch/>
+        <TextInput style={styles.SearchInput} placeholder="Search"></TextInput>
         </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
     homeContainer: {
- 
+      backgroundColor: '#F6F6F6',
       flex:1,
     
     },
@@ -35,4 +48,38 @@ const styles = StyleSheet.create({
         position:"absolute",
         right:10
       },
+      BarLocation: {
+        height:80,
+        color:"green",
+        position:"absolute",
+        alignContent:'center',
+        marginLeft:-190
+      },
+      locationContent:{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:-50
+      },
+      locationText:{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft:-10
+    
+      },
+      Search:{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+       
+    
+      },
+      SearchInput:{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:"#E6E7E9"
+    
+      }
   });
